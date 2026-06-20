9b3b83ecb87fd5b170514c385b937ec043914124
ajr-cabbage <alexander.rakotz@gmail.com>
2026-06-19 23:58:08 -0400

    CH:5 Video Streaming complete

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,19 +1,128 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+func getVideoAspectRatio(filePath string) (string, error) {
+
+	type vidData struct {
+		Streams []struct {
+			Index              int    `json:"index"`
+			CodecName          string `json:"codec_name,omitempty"`
+			CodecLongName      string `json:"codec_long_name,omitempty"`
+			Profile            string `json:"profile,omitempty"`
+			CodecType          string `json:"codec_type"`
+			CodecTagString     string `json:"codec_tag_string"`
+			CodecTag           string `json:"codec_tag"`
+			MimeCodecString    string `json:"mime_codec_string,omitempty"`
+			Width              int    `json:"width,omitempty"`
+			Height             int    `json:"height,omitempty"`
+			CodedWidth         int    `json:"coded_width,omitempty"`
+			CodedHeight        int    `json:"coded_height,omitempty"`
+			HasBFrames         int    `json:"has_b_frames,omitempty"`
+			SampleAspectRatio  string `json:"sample_aspect_ratio,omitempty"`
+			DisplayAspectRatio string `json:"display_aspect_ratio,omitempty"`
+			PixFmt             string `json:"pix_fmt,omitempty"`
+			Level              int    `json:"level,omitempty"`
+			ColorRange         string `json:"color_range,omitempty"`
+			ColorSpace         string `json:"color_space,omitempty"`
+			ColorTransfer      string `json:"color_transfer,omitempty"`
+			ColorPrimaries     string `json:"color_primaries,omitempty"`
+			FieldOrder         string `json:"field_order,omitempty"`
+			ID                 string `json:"id"`
+			RFrameRate         string `json:"r_frame_rate"`
+			AvgFrameRate       string `json:"avg_frame_rate"`
+			TimeBase           string `json:"time_base"`
+			StartPts           int    `json:"start_pts"`
+			StartTime          string `json:"start_time"`
+			DurationTs         int    `json:"duration_ts"`
+			Duration           string `json:"duration"`
+			BitRate            string `json:"bit_rate,omitempty"`
+			NbFrames           string `json:"nb_frames"`
+			ExtradataSize      int    `json:"extradata_size"`
+			Disposition        struct {
+				Default         int `json:"default"`
+				Dub             int `json:"dub"`
+				Original        int `json:"original"`
+				Comment         int `json:"comment"`
+				Lyrics          int `json:"lyrics"`
+				Karaoke         int `json:"karaoke"`
+				Forced          int `json:"forced"`
+				HearingImpaired int `json:"hearing_impaired"`
+				VisualImpaired  int `json:"visual_impaired"`
+				CleanEffects    int `json:"clean_effects"`
+				AttachedPic     int `json:"attached_pic"`
+				TimedThumbnails int `json:"timed_thumbnails"`
+				NonDiegetic     int `json:"non_diegetic"`
+				Captions        int `json:"captions"`
+				Descriptions    int `json:"descriptions"`
+				Metadata        int `json:"metadata"`
+				Dependent       int `json:"dependent"`
+				StillImage      int `json:"still_image"`
+				Multilayer      int `json:"multilayer"`
+			} `json:"disposition"`
+			Tags struct {
+				Language    string `json:"language"`
+				HandlerName string `json:"handler_name"`
+				Encoder     string `json:"encoder"`
+				Timecode    string `json:"timecode"`
+			} `json:"tags"`
+			SampleFmt      string `json:"sample_fmt,omitempty"`
+			SampleRate     string `json:"sample_rate,omitempty"`
+			Channels       int    `json:"channels,omitempty"`
+			ChannelLayout  string `json:"channel_layout,omitempty"`
+			BitsPerSample  int    `json:"bits_per_sample,omitempty"`
+			InitialPadding int    `json:"initial_padding,omitempty"`
+		} `json:"streams"`
+	}
+
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	vidDat := vidData{}
+	json.Unmarshal(out.Bytes(), &vidDat)
+
+	aspectRatio := vidDat.Streams[0].Width / vidDat.Streams[0].Height
+	switch aspectRatio {
+	case 16 / 9:
+		return "16:9", nil
+	case 9 / 16:
+		return "9:16", nil
+	default:
+		return "other", nil
+	}
+}
+
+func processVideoForFastStart(filePath string) (string, error) {
+	outFilePath := filePath + ".processing"
+	cmd := exec.Command("ffmpeg", "-i", filePath, "-c", "copy", "-movflags", "faststart", "-f", "mp4", outFilePath)
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return outFilePath, nil
+}
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
 	// Get video ID from request path
@@ -78,14 +187,42 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, err.Error(), err)
 		return
 	}
-	//make a unique filename and put the file to s3 bucket.
+	// process video for fast start
+	fmt.Println(tmpFile.Name())
+	procVideoFilepath, err := processVideoForFastStart(tmpFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error(), err)
+		return
+	}
+	processedFile, err := os.Open(procVideoFilepath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error(), err)
+		return
+	}
+	defer os.Remove(procVideoFilepath)
+	defer processedFile.Close()
+	// assign correct prefix based on aspect ratio, make a unique filename and put the file to s3 bucket.
+	aspectRatio, err := getVideoAspectRatio(procVideoFilepath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error(), err)
+		return
+	}
+	var prefix string
+	switch aspectRatio {
+	case "16:9":
+		prefix = "landscape"
+	case "9:16":
+		prefix = "portrait"
+	case "other":
+		prefix = "other"
+	}
 	randByte := make([]byte, 32)
 	rand.Read(randByte)
-	fileKey := fmt.Sprintf("%s.mp4", base64.RawURLEncoding.EncodeToString(randByte))
+	fileKey := fmt.Sprintf("%s/%s.mp4", prefix, base64.RawURLEncoding.EncodeToString(randByte))
 	cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket:      &cfg.s3Bucket,
 		Key:         &fileKey,
-		Body:        tmpFile,
+		Body:        processedFile,
 		ContentType: &mediaType,
 	})
 	// update video db entry with s3 url
